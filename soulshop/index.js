54ee43b709b26faa1117f1c1ee93e61475f0464e
jrcodev <jr.cod.dev@gmail.com>
2022-02-08 18:01:44 -0300

    Initial boilerplate

---
+++ soulshop/index.js
@@ -0,0 +1,18 @@
+require("dotenv/config");
+
+const express = require("express");
+const exprhbs = require("express-handlebars");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exprhbs.engine());
+app.set("view engine", "handlebars");
+
+const produtosRoutes = require("./routes/produtosRoutes");
+app.use(produtosRoutes);
+
+app.listen(3000);
