5ae8f527a26f37b769c3307abcbd57b5787b71b2
jrcodev <jr.cod.dev@gmail.com>
2022-02-08 14:52:55 -0300

    Middleware example

--- express01/index.js
+++ express01/index.js
@@ -6,10 +6,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
 const users = [];
 
+// Define um middleware
+function proibirHome(req, res, next) {
+  console.log("Acessando a home");
+  res.send("<h2>Proibido acessar</h2>");
+}
+
 // localhost:3000
-app.get("/", (req, res) => {
+app.get("/", proibirHome, (req, res) => {
   res.send(`
     <h1>Hello, Node!</h1>
     <a href="/usuario">NOVO USUÁRIO</a>
@@ -57,9 +68,13 @@ app.get("/usuario/:index", (req, res) => {
   }
 });
 
+// Esquema de rota de livros
+
 // O que fazer quando não encontra a rota:
 app.use((req, res, next) => {
   res.status(404).send("<h1>Página não encontrada</h1>");
 });
+
+
 
 app.listen(3000);
